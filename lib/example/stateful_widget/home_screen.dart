69a86f3a55573e6b4c584e3e4964bf6ab0f94420
hamada <hamada.devlop@gmail.com>
2022-03-26 05:58:14 +0200

    stateful widget

---
+++ lib/example/stateful_widget/home_screen.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    int counter = 0;
+    print('HAMADA HAMADA HAMADA');
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('StateFullWidget'),
+      ),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                setState((){
+                  counter--;
+                });
+              },
+              child: const Icon(
+                Icons.remove,
+              ),
+            ),
+            Text(
+              counter.toString(),
+              style: const TextStyle(
+                fontSize: 30,
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setState((){
+                  counter++;
+                });
+              },
+              child: const Icon(
+                Icons.add,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

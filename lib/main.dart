47231baf8d0b18adacc2c87cb2eb3e9f31cedce4
hamada <hamada.devlop@gmail.com>
2022-03-28 08:33:55 +0200

    counter screen_0

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:providerr/example/bloc_example/app_bloc_observer.dart';
 import 'package:providerr/example/bloc_example/bloc/counter_bloc.dart';
 import 'package:providerr/example/bloc_example/presentation/home_bloc_screen.dart';
 import 'package:providerr/example/inherited_widget/shop_card/logic/shopping_inherited.dart';
@@ -7,8 +8,15 @@ import 'package:providerr/example/inherited_widget/shop_card2/home_screen2.dart'
 
 import 'example/inherited_widget/shop_card/presentation/home_screen.dart';
 
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
 void main() {
-  runApp(const MyApp());
+  BlocOverrides.runZoned(
+        () => runApp(const MyApp()),
+    blocObserver: AppBlocObserver(),
+
+  );
 }
 
 class MyApp extends StatelessWidget {
